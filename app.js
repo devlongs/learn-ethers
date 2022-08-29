a66b9275cb0479ab0c0d7cc88365a3a3f20e4d3f
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-29 17:44:26 +0000

    code refactoring

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const providerUrl = process.env.PROVIDER_KEY;
 
 const contractAddress = "0x58659688bdF1c45fE0e78a11ff77b0d318C1b5c5";
 
-const getSomething = () => {
+const getTodo = async () => {
   // declare provider
   // connect provider
   //send action
   //get action
   const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-  const tokenContract = new ethers.Contract(contractAddress, abi, provider);
-  const data = tokenContract.getTodoItem(1).then((data) => console.log(data));
+  const todoContract = new ethers.Contract(contractAddress, abi, provider);
+  const todoItem = await todoContract.getTodoItem(1);
+  console.log(todoItem);
 };
 
-getSomething();
+getTodo();
