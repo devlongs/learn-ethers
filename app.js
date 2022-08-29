416b492b475f6af7ceca54be6282c6efca851d2d
Longs Pemun Gotar <developerlongs@gmail.com>
2022-08-29 17:55:21 +0000

    implemented try catch block

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const providerUrl = process.env.PROVIDER_KEY;
 const contractAddress = "0x58659688bdF1c45fE0e78a11ff77b0d318C1b5c5";
 
 const getTodo = async () => {
-  // declare provider
-  // connect provider
-  //send action
-  //get action
-  const provider = new ethers.providers.JsonRpcProvider(providerUrl);
-  const todoContract = new ethers.Contract(contractAddress, abi, provider);
-  const todoItem = await todoContract.getTodoItem(1);
-  console.log(todoItem);
+  try {
+    // declare provider
+    // connect provider
+    //send action
+    //get action
+    const provider = new ethers.providers.JsonRpcProvider(providerUrl);
+    const todoContract = new ethers.Contract(contractAddress, abi, provider);
+    const todoItem = await todoContract.getTodoItem(1);
+    console.log(todoItem);
+  } catch {
+    console.log("Something went wrong");
+  } finally {
+    console.log("This will run regardless");
+  }
 };
 
 getTodo();
